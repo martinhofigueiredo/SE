4cf1a7d575878c167127e2d9f080017fd07e920e
Martinho Figueiredo <up201506179@fe.up.pt>
2022-12-30 18:09:32 +0000

    Added Free rtos task for lora game

--- MatrixClockv0/src/main.cpp
+++ MatrixClockv0/src/main.cpp
@@ -4,14 +4,28 @@
 
 loraGame lora;
 
+TaskHandle_t loraHandle = NULL;
+
+void lora_task(void* parameter){
+    lora.game_routine();
+}
+
 void setup()
 {
     //alien.game_setup();
     lora.game_setup();
+    xTaskCreate(
+    lora_task,    // Function that should be called
+    "Lora Game Task",   // Name of the task (for debugging)
+    1000,            // Stack size (bytes) Need to know if is enough
+    NULL,            // Parameter to pass
+    1,               // Task priority
+    &loraHandle             // Task handle
+  );
 }
 
 void loop()
 {
     //alien.game_routine();
-    lora.game_routine();
+    //lora.game_routine();
 }
